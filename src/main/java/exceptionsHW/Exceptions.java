b9b046276ba6e00a217fb4235ae04e4589b08cc6
AlexeyShimko <shimko390@mail.ru>
2021-06-21 16:18:09 +0300

    Чистовик

--- src/main/java/exceptionsHW/Exceptions.java
+++ src/main/java/exceptionsHW/Exceptions.java
@@ -2,32 +2,35 @@ package exceptionsHW;
 
 public class Exceptions {
     public static void main(String[] args) throws MySizeArrayException, MyArrayDataException {
-
-            checkArray(new String[][]{
-                    {"56","4","g","8"},
-                    {"78","74","57","83"},
-
-                    {"98","45","h","82"}});
-
-
+        String [] [] arr = new String[][]{
+                {"56","4","5","8"},
+                {"78","74","57","83"},
+                {"78","74","57","83"},
+                {"98","45","d","82"}
+        };
+        try {
+            checkArray(arr);
+        }catch (MyArrayDataException e){
+            System.out.println("  Невозможно подсчитать сумму, введены некорректные данные!!!");
+        }
     }
     public static void checkArray(String[][] a) throws MySizeArrayException, MyArrayDataException {
-
         if (a.length != 4) {
             throw new MySizeArrayException("Ошибка! Размер массива должен быть 4х4");
         }
         int sum = 0;
-        for (int i = 0; i < a[i].length; i++) {
+        for (int i = 0; i < 4; i++) {
             if (a[i].length != 4) {
                 throw new MySizeArrayException("Ошибка! Размер массива должен быть 4х4");
             }
-            for (int j = 0; j < a.length; j++) {
-                try {
-                    sum += Integer.parseInt(a[i][j]);
-                } catch (NumberFormatException e){
-                    throw new MyArrayDataException(i,j);
-                }
+            for (int j = 0; j < 4; j++) {
+                    try {
+                        sum += Integer.parseInt(a[i][j]);
+                    } catch (NumberFormatException e) {
+                        throw new MyArrayDataException(i, j);
+                    }
             }
-        } System.out.println("Сумма = " + sum);
+            System.out.println(sum);
+        }
     }
 }
